94fbcb986772fec4215d76562e9758e203838d31
milnner <milnnernj@gmail.com>
2022-09-11 18:36:15 -0300

    Fixed the code, it was broken

--- ALGO ORDEM/c/heapsortForDoublyLinkedlist.h
+++ ALGO ORDEM/c/heapsortForDoublyLinkedlist.h
@@ -7,47 +7,52 @@ typedef struct item
     int id;
 }item;
 
-void criaHeap(linkedlist *list, long long i, long long f);
-void heapsort(linkedlist *list, long long n);
+void makeheapForDoublyLinkedlist(doublyLinkedlist *list, long long i, long long f);
+void heapsortForDoublyLinkedlist(doublyLinkedlist *list, long long n);
 
-void criaHeap(linkedlist *list, long long i, long long f)
+void makeHeapForDoublyLinkedlist(doublyLinkedlist *list, long long i, long long f)
 {
-    item* aux = (item *) return_an_item(i, list);
+    item *aux = return_an_item_from_doublyLinkedlist(i,list);
     long long j = (i * 2) + 1;
     while (j <= f)
     {
         if (j < f) 
         {
-            if (((item *)return_an_item(j, list))->id < ((item *)return_an_item(j+1, list))->id)
+            
+            if (
+                ((item *)return_an_item_from_doublyLinkedlist(j  ,list))->id    < 
+                ((item *)return_an_item_from_doublyLinkedlist(j+1,list))->id)
             {
-                j++;
+                j = j+1;
             }
         }
-        if (aux->id < ((item *)return_an_item(j, list))->id)
+        if (aux->id < ((item *)return_an_item_from_doublyLinkedlist(j  ,list))->id)
         {
-            swap_items(return_an_item(i,list),return_an_item(j, list), list);
+            ((doublyLinkedlist_node *)return_a_node_from_doublyLinkedlist(i,list))->item = return_an_item_from_doublyLinkedlist(j  ,list);
             i = j;
-            j = (2*i) + 1;
+            j = i*2 + 1;
         }
         else {
             j = f+1;
         }
     }
-    swap_items(return_an_item(i, list), aux,  list);
+    ((doublyLinkedlist_node *)return_a_node_from_doublyLinkedlist(i,list))->item = aux;
 }
 
-void heapsort(linkedlist *list, long long n)
+void heapsortForDoublyLinkedlist(doublyLinkedlist *list, long long n)
 {
-    for (long long i = (n-1)/2; i > 0; i--)
+    for (long long i = (n-1)/2; i >= 0; i--)
     {
-        criaHeap(list,i,n-1);
+        makeHeapForDoublyLinkedlist( list, i, n-1);
     }
-    for (long long i = n-1; i >= 1; i--)
+    for (long long i = n-1; i >= 1 ;i--)
     {
-        item *aux = (item *) return_an_item(i, list);
-        swap_items(return_an_item(0, list), return_an_item(i, list), list);
-        swap_items(return_an_item(i, list), aux, list);
-        criaHeap(list, 0, i-1);
+        swap_doublyLinkedlist_items(
+            return_a_node_from_doublyLinkedlist(i,list),
+            return_a_node_from_doublyLinkedlist(0,list),
+            list
+        );
+        makeHeapForDoublyLinkedlist(list, 0,i-1);
     }   
 }
 #endif
